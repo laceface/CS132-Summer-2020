2caee1b77165053ba1bfe16e1bfaafcf2d513b7f
Lacy Gomoluch <lacygomoluch@gmail.com>
2020-07-14 21:06:56 -0700

    Hop1

---
+++ LacyGomoluch/Module1/HOP1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("     /|");
+            Console.WriteLine("    / |");
+            Console.WriteLine("   /  |");
+            Console.WriteLine("  /   |");
+            Console.WriteLine(" /____|");
+
+            string Name = "Lacy";
+            int Age = 25;
+            bool isFemale = true;
+
+            Console.WriteLine(Name);
+            Console.WriteLine(Age);
+            Console.WriteLine(isFemale);
+        }
+    }
+}
